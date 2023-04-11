8a7824f3a15540cbd725d5b8cb417e3194e32b4a
Breno <brennoohudson@gmail.com>
2023-04-11 17:32:56 -0300

    trocando de pc

--- src/app.js
+++ src/app.js
@@ -2,28 +2,42 @@ import express from "express"
 import cors from "cors"
 
 const app = express();
-app(cors())
+app.use(cors());
+app.use(express.json());
 
 let participantes = []
 
-app.post("participants", (req, res)=>{
-
+app.post("/participants", (req, res)=>{
+    const { name } = req.body
+    
+    if(name.includes(participantes)){
+        res.status(409).send("Esse Usuario ja existe!")
+    } else {
+        if(name == []){
+            res.status(422).send("Necessario informar dados")
+        } else {
+            res.status(201).send("Cadastrar participante no MongoDB ,{ name: 'xxx', lastStatus: Date.now() }")
+        }
+    }
+    console.log(name)
+    res.send(name)
 })
 
-app.get("participants", (req, res)=>{
+app.get("/participants", (req, res)=>{
     res.send(participantes)
 })
 
-app.post("messages", (req, res)=>{
-    let novaMensagem = {to:req.body.to , text:req.body.text, type:req.body.type}
-    res.send(novaMensagem)
+app.post("/messages", (req, res)=>{
+    const {to, text, type} = req.body
+    const {User} = req.header
+    
 })
 
-app.get("messages", (req, res)=>{
+app.get("/messages", (req, res)=>{
     res.send("oi")
 })
 
-app.post("status", (req, res)=>{
+app.post("/status", (req, res)=>{
     //req.header({user})
     //if(isHeader){
         res.send("")
